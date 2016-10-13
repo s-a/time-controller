c9cd75c3c366b7491ca8fae3dcff5c685760f422
s-a <stephan.ahlf@gmail.com>
2016-10-13 13:47:51 +0200

    add tests

--- test/index.js
+++ test/index.js
@@ -1,10 +1,49 @@
 'use strict';
 
 var assert = require('assert');
-var timeController = require('../lib');
+var TimeController = require('../lib');
+var ContextObject = function(){
+this.test = true;
+return this;
+}
 
 describe('time-controller', function () {
-  it('should have unit test!', function () {
-    assert(true, 'we expected this package author to add actual unit tests.');
-  });
+
+	it('should execute debounced', function (done) {
+		this.timeout(50);
+		var contextObject = new ContextObject()
+		var debouncedTimeController = new TimeController(function(){
+			assert(true, this.test === true);
+			done();
+		}, contextObject).debounce(10);
+
+		debouncedTimeController();
+	}); 
+
+	it('should execute throttled', function (done) {
+		this.timeout(50);
+		var contextObject = new ContextObject()
+		var throttledTimeController = new TimeController(function(){
+			assert(true, this.test === true);
+			done();
+		}, contextObject).throttle(10);
+
+		throttledTimeController();
+		throttledTimeController();
+		throttledTimeController();
+		throttledTimeController();
+		throttledTimeController();
+	});
+
+	it('should execute immediate', function (done) {
+		this.timeout(300);
+		var contextObject = new ContextObject()
+		var immediateTimeController = new TimeController(function(){
+			assert(true, this.test === true);
+			done();
+		}, contextObject).immediate(100);
+
+		immediateTimeController();
+	});
+
 });
